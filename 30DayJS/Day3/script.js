8cf3a4fb261c176c0c744d87f63f31718294c334
Blooobs <trungndweb.vn@gmail.com>
2023-08-14 15:15:28 +0700

    update

--- 30DayJS/Day3/script.js
+++ 30DayJS/Day3/script.js
@@ -1,3 +1,17 @@
 console.log("|-----------|")
 console.log("|---DAY 3---|")
 console.log("|-----------|")
+
+console.log("")
+console.log("\"LEVEL 1\"")
+console.log("")
+
+console.log("Ex1: ")
+
+console.log("")
+console.log("\"LEVEL 2\"")
+console.log("")
+
+console.log("")
+console.log("\"LEVEL 3\"")
+console.log("")
